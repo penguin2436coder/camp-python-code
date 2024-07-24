3d0bba8962afb653315d170b837e2812b84e1fe3
penguin2436coder <penguin2436coder@penguincoding.org>
2024-07-24 10:52:15 -0400

    Add files via upload
    Camp Days 1&2, Initial commit.

---
+++ Camp Day 2.py
@@ -0,0 +1,55 @@
+#calulator
+'''num1 = int(input("Give me a number\n"))
+num2 = int(input("Give me another number\n"))
+math = input("Type what math symbol you want\n")
+if math == "+" : print("\n" + str(num1) + " + " + str(num2) + " = "+ str(num1 + num2))
+elif math == "-" : print("\n" + str(num1) + " - " + str(num2) + " = "+ str(num1 - num2))
+elif math == "*" : print("\n" + str(num1) + " * " + str(num2) + " = "+ str(num1 * num2))
+elif math == "/" : print("\n" + str(num1) + " / " + str(num2) + " = "+ str(num1 / num2))
+else : print("That is not a math symbol")
+
+#shop base idea
+coins = int(input("How many coins do you have?\n:"))
+item = input("What item do you want to buy?\n:")
+if item == "hat" :
+    if coins >= 5 :
+        coins -= 5
+        print("You bought a hat and you now have " + str(coins) + " coin(s)")
+    else : print("You don't have enough coins to buy a hat")
+if item == "sword" :
+    if coins >= 10 :
+        coins -= 10
+        print("You bought a sword and you now have " + str(coins) + " coin(s)")
+    else : print("You don't have enough coins to buy a sword")
+
+print(coins)'''
+
+#critical hit system
+'''import random
+crit = (random.randint(1, 8))
+if crit == 1 : print("you crit")
+else : print("you did not crit")'''
+
+
+'''import random
+print(random.randint(1, 1025))
+
+
+vulpix
+totadile
+zorua
+mew
+kleavor
+scrafty
+
+
+maushold
+cleffa
+jigglypuff
+venusaur
+staravia
+terrakion'''
+
+
+
+
